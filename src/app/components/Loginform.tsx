98f88992f595f197b3679deeb4238eb99b5ed95f
Kevin Bollman <digitalkdogg@gmail.com>
2025-06-01 08:57:04 -0500

    form validation

--- src/app/components/Loginform.tsx
+++ src/app/components/Loginform.tsx
@@ -8,7 +8,6 @@ import { insert } from '@/common/dbutils'
 
 export default function LoginPage() {
   const router = useRouter()
-
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
 
@@ -16,48 +15,80 @@ export default function LoginPage() {
     const username = formData.get('username')
     const password = formData.get('password')
 
-    setCookie('sicher', encrypt('user:' + username + '||pass:' + password), {maxAge:512, secure:true, sameSite: 'strict'})  
+    if (validateForm(username, password)) {
 
-   const response = await fetch('/api/login/auth', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    })
+      setCookie('sicher', encrypt('user:' + username + '||pass:' + password), {maxAge:512, secure:true, sameSite: 'strict'})  
 
-    if (response.ok) {
-      const json = await response.json()
-      const loginerror = document.querySelector('.login-error')
+      const response = await fetch('/api/login/auth', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+      })
+
+      if (response.ok) {
+        const json = await response.json()
+        const loginerror = document.querySelector('.login-error')
       if (json.data.length > 0 ) {
 
-         // const loginerror = document.querySelector('.login-error')
-          if (loginerror) {
-            loginerror.classList.remove('error')
-            loginerror.classList.remove('notvisible')
-          }
-          router.push('/')
+        // const loginerror = document.querySelector('.login-error')
+        if (loginerror) {
+          loginerror.classList.remove('error')
+          loginerror.classList.remove('notvisible')
+        }
+        router.push('/')
       } else {
-          if (loginerror) {
-            loginerror.innerHTML = 'The user name and password are incorrect'
-            loginerror.classList.add('error');
-            loginerror.classList.remove('notvisible')
-          }
+        if (loginerror) {
+          loginerror.innerHTML = 'The user name and password are incorrect'
+          loginerror.classList.add('error');
+          loginerror.classList.remove('notvisible')
+        }
       } 
-
-     // router.push('/')
-    } else {
-      // Handle errors
     }
+  } else {
+  // Handle errors
   }
+
+  function validateForm(username:any, password:any) {
+    document.querySelector('.username-error')?.classList.add('notvisible')
+    document.querySelector('.password-error')?.classList.add('notvisible')
+
+    if (username.length > 2 && password.length > 2) {
+      return true
+    } else {
+      if (username.length <= 2 ) {
+        document.querySelector('.username-error')?.classList.remove('notvisible')
+      }
+
+      if (password.length <=2 ) {
+        document.querySelector('.password-error')?.classList.remove('notvisible')
+      }
+    }
+    return false;
+  }
+}
+
+const hideErrorMsg = (e:any) => {
+  const parent = e.currentTarget.getAttribute('data-error');
+  document.querySelector('.'+parent)?.classList.add('notvisible')
+}
+
+  
  
   return (
     <form onSubmit={handleSubmit} className = "-translate-y-50" >
-     <div className = "flex my-5">
-        <label htmlFor="username">Username : </label>
-        <input id = "username" name="username" type = "text"  />
+     <div className = "flex flex-col">
+        <p className = "error username-error notvisible">Username is requried</p>
+        <div className = "wrap">
+          <label htmlFor="username">Username : </label>
+          <input id = "username" name="username" data-error = "username-error" type = "text" onFocus={hideErrorMsg} />
+        </div>
       </div>
 
-      <div className = "flex my-10">
-        <label htmlFor="password">Password : </label>
-        <input id = "password" name="password" type = "password"  />
+      <div className = "flex my-5 flex-col">
+         <p className = "error password-error notvisible">Password is requried</p>
+         <div className = "wrap">
+            <label htmlFor="password">Password : </label>
+            <input id = "password" name="password" data-error="password-error" type = "password" onFocus={hideErrorMsg}   />
+          </div>
       </div>
       
       <br />
