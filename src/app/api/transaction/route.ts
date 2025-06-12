478ea6fcac62cf470d5c975e2cda10d4d263f3e9
Kevin Bollman <digitalkdogg@gmail.com>
2025-06-12 18:02:00 -0500

    made transaction api a thing

--- src/app/api/transaction/route.ts
+++ src/app/api/transaction/route.ts
@@ -1,5 +1,10 @@
 import { NextResponse, NextRequest } from 'next/server'
+import {cookies} from 'next/headers'
 import pool from '@/common/db';
+import {select} from '@/common/dbutils'
+import {decrypt, encrypt} from '@/common/crypt'
+import { getDataFromCookie} from '@/common/common'
+import {doesSessionExists} from '@/common/session'
 
 export async function GET(request: NextRequest) {
 
@@ -38,4 +43,37 @@ export async function GET(request: NextRequest) {
     }
     return NextResponse.json(response, { status: 401 })
  }
+}
+
+export async function POST(request:NextRequest) {
+  const cookieStore = cookies()
+  const cookiename:any = process.env.NEXT_PUBLIC_cookiestr
+  const sessionCookie = (await cookieStore).get(cookiename)
+
+  if (sessionCookie) {
+     // if (await doesSessionExists(data.user, sessionCookie.value)) {
+     //     return NextResponse.json({'status': 'success', 'msg': 'Session is valid already'})
+     // } else {
+     //     return NextResponse.json({'status': false, 'msg': 'We can not login in at this time.  Try clearing your cache and try again.'})
+     // }
+    const cdata = (await cookieStore).get('sicher')
+    const cookiestr = decrypt(cdata?.value);
+    const data = getDataFromCookie(cookiestr)
+
+
+    const query = {
+      select : '*',
+      from : 'Transactions', 
+      where: 'trans_Id = "' + data.trans_id + '"',
+      limit: 1
+    }
+
+    try {
+      const response = await select(query);
+      return NextResponse.json({response})
+    } catch (e) {
+      return NextResponse.json({e})
+    }
+    return 
+  }
 }
