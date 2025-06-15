5a89d84e302e77fdbddad3ebaf97a433f4b4e7db
Kevin Bollman <digitalkdogg@gmail.com>
2025-06-15 13:59:43 -0500

    crypt working again

--- src/common/crypt.ts
+++ src/common/crypt.ts
@@ -5,11 +5,11 @@ const cipherKey:any = process.env.NEXT_PUBLIC_APP_crypt
 export const encrypt = (data:any) => {
   const encrypted = CryptoJS.AES.encrypt(data, cipherKey).toString();
   process.stdout.write('\n' +'after enc val: ' + encrypted + '\n')
-  return encodeURIComponent(encrypted);
+   return encodeURIComponent(encrypted);
 };
 
 export const decrypt = (encryptedData:any) => {
-  const decoded = decodeURIComponent(encryptedData);
+ const decoded = decodeURIComponent(encryptedData);
   const bytes = CryptoJS.AES.decrypt(decoded, cipherKey);
   process.stdout.write('\n' +'after decrypt val now: ' + bytes + '\n')
 
