57ac2258c925bc9b00e90ac93b2f2f557e5d0e3f
Kevin Bollman <digitalkdogg@gmail.com>
2025-06-22 18:53:44 -0500

    added cancel on search dropdown

--- src/app/components/Dropdown.tsx
+++ src/app/components/Dropdown.tsx
@@ -93,16 +93,20 @@ function Dropdown(prop:Props) {
 
         wrapper?.classList.add('sendtofront');
 
+        results?.classList.remove('hide');
+
         results_eles.forEach( el => {
             el.classList.remove('hide')
         })
 
-        results?.classList.remove('hide');
-
         if (results.classList.contains('hide')) {
-            arrow.classList.remove('rotate-270')    
+            if (arrow.classList.contains('rotate-270')) {
+                arrow.classList.remove('rotate-270')    
+            }
         } else {
-            arrow.classList.add('rotate-270')
+            if (arrow.classList.contains('rotate-270') == false) {
+                arrow.classList.add('rotate-270')
+            }
         }
 
         if (dropdownInput) {
@@ -143,6 +147,25 @@ function Dropdown(prop:Props) {
 
     }
 
+    const arrowclick = (event) => {
+        const ele = event.target
+        const dropdownInput = document.getElementById(prop.type + '_dropinput');
+        const results = document.querySelector('#' + prop.type + '_results')
+     
+        if (dropdownInput.value.length>0) {
+            dropdownInput.value = ''
+        }
+        
+        if (ele.classList.contains('rotate-270')) {
+            
+            setTimeout(function () {
+                ele.classList.remove('rotate-270')
+                results.classList.add('hide')
+            },301)
+        }
+
+    }
+
     const getPlaceholder = () => {
         if (prop.val == '') {
             return 'Select Text'
@@ -171,7 +194,7 @@ function Dropdown(prop:Props) {
                     onChange={handleInputChange}
                 />
                 <div className = {styles.selectValue} id = {prop.type + '_selectValue'}>{''}</div>
-                <div className = {'arrow ' + styles.droparrow} id = {prop.type + '_arrow'}>
+                <div className = {'arrow ' + styles.droparrow} id = {prop.type + '_arrow'} onClick={(e) => arrowclick(e)}>
                     <Svg type = 'downarrow' />
                 </div>
             </div>
