457ebfdb59c0ac975acdc00b152c7ff003726356
Kevin Bollman <digitalkdogg@gmail.com>
2025-06-22 18:04:07 -0500

    add action to add submit

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -5,6 +5,7 @@ import Input from '@/app/components/Input'
 import Dropdown from '@/app/components/Dropdown'
 import { Geist } from 'next/font/google'
 import { useState, useEffect } from 'react';
+import {redirect} from 'next/navigation'
 import '@/app/globals.css'
 
 const geist = Geist({
@@ -17,31 +18,43 @@ const geist = Geist({
 
 export default function Page() {
 
+    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+
+        redirect('/save');
+    }
+
     return (
 
         <div className = {geist.className}>
             <main className = "flex" id = "login-main">
                 <Leftside enable = {true} />
                 <div className = "flex-3 bg-white flex justify-center-safe items-center-safe px-20 flex-col ">
-                    <div className = "flex flex-row justify-between py-5">
-                        <span>Date :</span>
-                        <Input id = "date" name = "date" val = '' disabled = {false} />
-                    </div>
+                    <form onSubmit={handleSubmit} > 
+                        <div className = "flex flex-row justify-between py-5">
+                            <span>Date :</span>
+                            <Input id = "date" name = "date" val = '' disabled = {false} />
+                        </div>
 
-                    <div className = {'flex flex-row justify-between py-5'}>
-                        <span>Company : </span>
-                        <Dropdown val = '' api = "../api/clients" type = 'clients' />
-                    </div>
+                        <div className = {'flex flex-row justify-between py-5'}>
+                            <span>Company : </span>
+                            <Dropdown val = '' api = "../api/clients" type = 'clients' />
+                        </div>
 
-                    <div className = "flex flex-row justify-between py-5">
-                        <span>Amount :</span>
-                        <Input id = "amount" name = "date" val = '' disabled = {false} />
-                    </div>
+                        <div className = "flex flex-row justify-between py-5">
+                            <span>Amount :</span>
+                            <Input id = "amount" name = "date" val = '' disabled = {false} />
+                        </div>
 
-                    <div className = {'flex flex-row justify-between py-5 '}>
-                        <span>Category : </span>
-                        <Dropdown val = '' api = "../api/categories" type = 'categories' />
-                    </div>
+                        <div className = {'flex flex-row justify-between py-5 '}>
+                            <span>Category : </span>
+                            <Dropdown val = '' api = "../api/categories" type = 'categories' />
+                        </div>
+
+                        <div className= "flex justify-center-safe mb-20">
+                            <button className="inset-shadow-indigo-500 mr-5" type="submit">Submit</button>
+                            <button className="ml-5" type="reset">Reset</button>
+                        </div>
+                    </form>
                 </div>
             </main>
         </div>
