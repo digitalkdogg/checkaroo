40a857b3384627b5b3085f0eed33f8b22b323630
Kevin Bollman <digitalkdogg@gmail.com>
2025-06-27 14:06:03 -0500

    added loading spinner to inital checksession login

--- src/app/components/Loginform.tsx
+++ src/app/components/Loginform.tsx
@@ -3,41 +3,57 @@ import { FormEvent } from 'react'
 import { useRouter } from 'next/router'
 import { setCookie,getCookie } from 'cookies-next';
 import { encrypt } from '@/common/crypt';
+import { useState, useEffect } from 'react';
+import  Loading  from '@/app/components/Loading';
 
 export default function LoginPage() {
 
-  const checkSession = async () => {
-       const response = await fetch('/api/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-      })
-
-    if (response.ok) {
-      const json = await response.json()
-      if(json.valid ==  true) {
-          router.push('/')
-      }
-
-    }
-}
   
   const router = useRouter()
 
-  const cookiename:any = process.env.NEXT_PUBLIC_cookiestr
-  const sessionCookie = getCookie(cookiename)
-  if (sessionCookie) {
-    checkSession()
-  }
+  const [loginData, setLoginData] = useState<any>([])
+  const [isLoginLoading, setIsLoginLoading] = useState(true);
+  const [loginError, setLoginError] = useState<any>(null);
 
-  setTimeout(function () {
-    try {
-      const overlay = document.getElementById("overlay")
-      if (overlay) {
-        overlay.classList.add('hide')
+
+    useEffect(() => {
+
+      const checkSession = async () => {
+        try {
+          const response = await fetch('/api/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+          })
+        
+          if (response.ok) {
+            const json = await response.json()
+            console.log('Session check response:', json);
+            if(json.valid ==  true) {
+              // setIsLoginLoading(false);
+              router.push('/')
+            } 
+
+          }
+        } catch(err) {
+          console.error('Error checking session:', err);
+          setLoginError(err);
+        } finally {
+           
+        }
+      
       }
-    } catch(e) {}
-  },1000)
-  
+
+      const cookiename:any = process.env.NEXT_PUBLIC_cookiestr
+      const sessionCookie = getCookie(cookiename)
+
+      if (sessionCookie) {
+        checkSession();
+      } else {
+        setIsLoginLoading(false);
+        // You can add any additional logic here if needed
+      }
+    }, []);
+
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
 
@@ -45,7 +61,6 @@ export default function LoginPage() {
     const username = formData.get('username')
     const password = formData.get('password')
 
-    console.log(password);
 
     if (validateForm(username, password)) {
       setCookie('sicher', 
@@ -111,8 +126,23 @@ const hideErrorMsg = (e:any) => {
   document.querySelector('.'+parent)?.classList.add('notvisible')
 }
 
-  
- 
+  if (isLoginLoading) {
+    return (
+      <div className="flex-3 bg-white flex px-20 flex-col items-center">
+        <Loading size={24} />
+      </div>
+    );
+  }
+
+  if (loginError) { 
+    return (
+      <div className="flex-3 bg-white flex px-20 flex-col my-50 items-center">
+        <p>Error: {loginError.message}</p>
+      </div>
+    );
+  }  
+   
+    
   return (
     <form onSubmit={handleSubmit} className = "-translate-y-50" >
      <div className = "flex flex-col justify-center-safe">
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,7 @@
 
 import { Geist } from 'next/font/google'
 import Leftside from '@/app/components/Leftside'
-import Login from '@/app/components/Loginform'
+import LoginPage from '@/app/components/Loginform'
 import '@/app/globals.css'
 
 
@@ -14,12 +14,12 @@ export default function Page() {
  
     return (
         <div className = {geist.className}>
-            <div id = "overlay"></div>
+
             <main className = "flex" id = "login-main">
                 <Leftside enable = {false} />
                 <div className = "flex-3 bg-white flex justify-center-safe items-center-safe px-20 flex-col ">
                     <h2 className = "text-center my-20 -translate-y-50">Please login to use the Checkaroo Web Application and hop into your banking</h2>
-                    <Login />
+                    <LoginPage />
                 </div>
             </main>
         </div>
