058fa4cf13351ca6c1b2ea41f3d24667565cd73e
Kevin Bollman <digitalkdogg@gmail.com>
2025-06-28 11:02:29 -0500

    add checksession comp to login

--- src/app/components/ChecksessionComp.tsx
+++ src/app/components/ChecksessionComp.tsx
@@ -1,25 +1,62 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
+import { redirect } from 'next/navigation'
+import { useState, useEffect } from 'react';
+import Leftside from '@/app/components/Leftside'
+import Loading from '@/app/components/Loading';
+import { readCookie } from '@/common/cookieServer';
 
-const checkRedirect = () =>{
-    //redirect('/') 
-    const router = useRouter();
-    return router.replace('/'); // Redirect to the home page
+
+const checkRedirect = async (setLoginData:any, setIsLoginLoading:any) =>{
+
+    const cookiename:any = process.env.NEXT_PUBLIC_cookiestr;
+    var sessionCookie = await readCookie(cookiename);
+
+    if (!sessionCookie) {
+        setIsLoginLoading(false);
+        return; // No session cookie, do not redirect
+    }
+    
+    const response = await fetch('/api/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+         })
+
+    if (response.ok) {
+        const json = await response.json()
+        if(json.valid ==  true) {
+            return redirect('/'); // Redirect to the home page
+        } else {
+            setLoginData(true);
+            setIsLoginLoading(false);
+            console.log('I stay right here');
+        }
+    }
 } 
 
 
 
 export default function ChecksessionComp() {
+      const [loginData, setLoginData] = useState<any>(false)
+      const [isLoginLoading, setIsLoginLoading] = useState(true);
+      const [loginError, setLoginError] = useState<any>(null);
+          
+    useEffect(() => {
+      checkRedirect(setLoginData, setIsLoginLoading)
+    }, []);
 
+    if (isLoginLoading) {
+        return (
+            <div>
+                <main className = "flex">
+                    <Leftside enable = {true} />
+                    <div className = "flex-3 bg-white flex items-center justify-center" >
+                        <Loading size={24} />
+                    </div>
+                </main>
+            </div>
+        )
+    }
 
-          checkRedirect()
-
-
-
-  return (
-    <div>
-        testing session 
-    </div>
-  )
+  return (<div></div>)
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,9 +3,7 @@
 import { Geist } from 'next/font/google'
 import Leftside from '@/app/components/Leftside'
 import LoginPage from '@/app/components/Loginform'
-import { useState, useEffect } from 'react';
-import { redirect } from 'next/navigation'
-import Loading from '@/app/components/Loading';
+import ChecksessionComp from '../components/ChecksessionComp';
 
 import '@/app/globals.css'
 
@@ -15,71 +13,10 @@ const geist = Geist({
 })
 
 export default function Page() {
-    const [loginData, setLoginData] = useState<any>(false)
-    const [isLoginLoading, setIsLoginLoading] = useState(true);
-    const [loginError, setLoginError] = useState<any>(null);
-
-    const checkSession = async () => {
-        try {
-          const response = await fetch('/api/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-         })
-        
-          if (response.ok) {
-            const json = await response.json()
-            if(json.valid ==  true) {
-                setLoginData(true)
-            } else {
-                setIsLoginLoading(false);
-            }
-
-          }
-        } catch(err) {
-            setLoginError(err);
-            setIsLoginLoading(false);
-        } 
-      return false;
-      }
-
-
-
-    useEffect(() => {
-       checkSession()  
-    }, []);
-
-    if (loginData) {
-        redirect('/');
-    }
-
-    if (isLoginLoading) {
-        return (
-            <div className = {geist.className}>
-                <main className = "flex">
-                    <Leftside enable = {true} />
-                    <div className = "flex-3 bg-white flex items-center justify-center" >
-                        <Loading size={24} />
-                    </div>
-                </main>
-            </div>
-        )
-    }
-
-    if (loginError) {
-        return (
-            <div className = {geist.className}>
-                <main className = "flex">
-                    <Leftside enable = {false} />
-                    <div className = "flex-3 bg-white flex px-20 flex-col my-50 items-center">
-                        Error: {loginError.message}
-                    </div>
-                </main>
-            </div>
-        );
-    }   
-
+ 
     return (
         <div className = {geist.className}>
+            <ChecksessionComp />
 
             <main className = "flex" id = "login-main">
                 <Leftside enable = {false} />
