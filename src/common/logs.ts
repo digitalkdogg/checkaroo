f9df9c5850305d86c423ecc1f7e423b5b8887bfa
Kevin Bollman <digitalkdogg@gmail.com>
2025-07-12 14:09:20 -0500

    support for logs to accept an object or string

--- src/common/logs.ts
+++ src/common/logs.ts
@@ -1,10 +1,22 @@
-export const writelog = (msg:string, premsg?:string) => {
+export const writelog = (msg:string | object, premsg?:string) => {
+
     try {
-        if (premsg) {
-            msg = premsg + '\n' + msg
-        } 
-        process.stdout.write('\n' + msg.toString() + '\n')
-    } catch (e) {
-        return e;
+        if (typeof msg == 'object') {
+            if (!premsg) {
+                const premsg = '';
+            }
+           const keys = Object.keys(msg)
+           process.stdout.write('\n' + premsg);
+           keys.forEach((val, index)=> {
+            process.stdout.write('\n' + val + '\n' )
+           })
+        } else {
+             if (premsg) {
+                msg = premsg + '\n' + msg
+            } 
+            process.stdout.write('\n' + msg.toString() + '\n')
+        }
+    } catch (error) {
+       return;
     }
 }
