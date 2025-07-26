35ff4115afba4141f6d0fb3adc9ee611a0cf35fe
Kevin Bollman <digitalkdogg@gmail.com>
2025-07-26 18:22:11 -0500

    all new balance component

--- src/app/components/Balance.tsx
+++ src/app/components/Balance.tsx
@@ -15,55 +15,52 @@ interface BalanceResponse {
   balance: number;
 }
 
-
-
 export default function Page({ enable = true, session }: Props) {
-  const [balance, setBalance] = useState<number>();
-  const [isLoading, setIsLoading] = useState(true);
+    const [balance, setBalance] = useState<number|undefined>();
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    if (!session) return;
-
-    const fetchBalance = async () => {
-      try {
-        const response = await fetch('/api/balance', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ session: superEcnrypt(session) }),
-        });
-
-        const json: BalanceResponse[] = await response.json();
-
-        if (response.ok && Array.isArray(json) && json.length > 0) {
-          animateBalance(json[0].balance)
-        } else {
-          setBalance(0);
+    const fetchBalance = async(startBalance?:number) => {
+        try {
+            const response = await fetch('/api/balance', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ session: superEcnrypt(session) }),
+            });
+            
+            const json: BalanceResponse[] = await response.json();
+            
+            if (response.ok && Array.isArray(json) && json.length > 0) {
+                if (startBalance) {
+                    animateBalance(Number(startBalance), json[0].balance)
+                } else {
+                    setBalance(json[0].balance)
+                }
+            } else {
+                setBalance(0);
+            }
+        } catch {
+            setBalance(0);
+        } finally {
+            setIsLoading(false);
         }
-      } catch {
+    }
 
-        setBalance(0);
-      } finally {
-        setIsLoading(false);
-      }
+    const getBalance = async() => {
+        const cookieBalance = await readCookie('balance');
+        if (cookieBalance) {
+            fetchBalance(Number(cookieBalance));
+        } else {
+           fetchBalance();
+        }
     };
 
-    const getCookieBalance = async (bal:number): Promise<number> => {
-      const cookieBalance = await readCookie('balance');
-      if (cookieBalance) {
-        const parsed = parseFloat(cookieBalance);
-        return isNaN(parsed) ? 0 : parsed;
-      } else {
-        return bal;
-      }
-    };
-
-    const animateBalance = async (endBalance: number) => {
-        let newBalance = await getCookieBalance(endBalance); 
+    const animateBalance = async (startBalance:number, endBalance: number) => {
+        let newBalance = Number(startBalance) 
         setBalance(newBalance);
 
         const balinterval = setInterval(() => {
-            if ((newBalance + 100) < endBalance) {
-                newBalance += 100;
+            if ((newBalance + 50) < endBalance) {
+                newBalance += 50;
                 setBalance(newBalance);
             } else {
                 setBalance(endBalance);
@@ -71,29 +68,30 @@ export default function Page({ enable = true, session }: Props) {
                 deleteCookie('balance')
             }
         }, 100);
-    };
+    }
 
-    fetchBalance();
-  }, [session]);
+    useEffect(() => {
+        getBalance();
+    }, [session]);
 
-  if (!enable) return null;
+    if (!enable) return null;
 
-  return (
-    <div className={styles.wrap}>
-      <div className={styles.circle}>
-        {isLoading ? (
-          <span className="relative size-30 left-3 top-3">
-            <Loading />
-          </span>
-        ) : (
-          <>
-            <div className={styles.label}>Balance</div>
-            <div className={styles.number}>
-              <span>${(balance ?? 0).toFixed(2)}</span>
+    return (
+        <div className={styles.wrap}>
+            <div className={styles.circle}>
+                {isLoading ? (
+                <span className="relative size-30 left-3 top-3">
+                    <Loading />
+                </span>
+                ) : (
+                <>
+                    <div className={styles.label}>Balance</div>
+                    <div className={styles.number}>
+                    <span className = "min-h-[34px]">{balance && <span>${(balance).toFixed(2)}</span>}</span>
+                    </div>
+                </>
+                )}
             </div>
-          </>
-        )}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
