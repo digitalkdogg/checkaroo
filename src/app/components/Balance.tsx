a52404c86cbc404ba296fbb715dc402da77747d7
Kevin Bollman <digitalkdogg@gmail.com>
2025-08-01 18:19:09 -0500

    set trend to neg if balance is below zero

--- src/app/components/Balance.tsx
+++ src/app/components/Balance.tsx
@@ -31,7 +31,9 @@ export default function Page({ enable = true, session }: Props) {
       const json: BalanceResponse[] = await response.json();
 
       if (response.ok && json.length > 0) {
-        const newBalance = Number(json[0].balance).toFixed(2);
+        let newBalance = Number(json[0].balance).toFixed(2);
+
+
         if (typeof oldBalance === 'number') {
             if (oldBalance < Number(newBalance)) {
                 animateBalance(oldBalance, Number(newBalance), 'pos');
@@ -39,7 +41,10 @@ export default function Page({ enable = true, session }: Props) {
                 animateBalance(oldBalance, Number(newBalance), 'neg');
             }
         } else {
-            setBalance(Number(newBalance));
+          if (Number(newBalance) < 0) {
+            setTrend('neg');
+          }
+          setBalance(Number(newBalance));
         }
       } else {
         setBalance(0);
@@ -80,6 +85,9 @@ export default function Page({ enable = true, session }: Props) {
           setTrend('');
           setBalance(end);
           deleteCookie('balance');
+          if (end < 0) {
+            setTrend('neg');
+          }
           clearInterval(interval)
         }
       } else {
@@ -89,9 +97,13 @@ export default function Page({ enable = true, session }: Props) {
           setTrend('')
           setBalance(end);
           deleteCookie('balance');
+          if (end < 0) {
+            setTrend('neg');
+          }
           clearInterval(interval)
         }
       }
+
     },200)
 
   }
