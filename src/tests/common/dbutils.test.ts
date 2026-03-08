6a8323e23d54e2b078b48bbfdfe6ac3b1faa09a3
Kevin Bollman <digitalkdogg@gmail.com>
2026-03-08 10:33:38 -0500

    Update dbutils.test.ts

--- src/tests/common/dbutils.test.ts
+++ src/tests/common/dbutils.test.ts
@@ -23,11 +23,11 @@ describe('dbutils', () => {
       from: 'table1',
       where: 'id=1'
     }
-    mockConnection.query.mockResolvedValue([[{ col1: 'val1' }]])
+    mockConnection.execute.mockResolvedValue([[{ col1: 'val1' }], []])
     
     const result = await select(query)
     expect(result).toEqual([{ col1: 'val1' }])
-    expect(mockConnection.query).toHaveBeenCalledWith('select col1 from table1 where id=1')
+    expect(mockConnection.execute).toHaveBeenCalledWith('select col1 from table1 where id=1', [])
   })
 
   test('insert', async () => {
@@ -36,24 +36,25 @@ describe('dbutils', () => {
       fields: ['f1', 'f2'],
       vals: ['v1', 'v2']
     }
-    mockConnection.execute.mockResolvedValue({ insertId: 1 })
+    mockConnection.execute.mockResolvedValue([{ insertId: 1 }, []])
     
     const result = await insert(query)
     expect(result).toEqual({ data: { insertId: 1 } })
-    expect(mockConnection.execute).toHaveBeenCalledWith('insert into table1(f1,f2)values(?,?)', ['v1', 'v2'])
+    expect(mockConnection.execute).toHaveBeenCalledWith('insert into table1 (f1, f2) values (?, ?)', ['v1', 'v2'])
   })
 
   test('update', async () => {
     const query = {
       table: 'table1',
       fields: 'f1="v1"',
+      fieldVals: [],
       where: 'id=1'
     }
     mockConnection.execute.mockResolvedValue([{ affectedRows: 1 }, []])
     
     const result = await update(query)
     expect(result).toEqual({ data: { affectedRows: 1 } })
-    expect(mockConnection.execute).toHaveBeenCalledWith('update table1 set f1="v1" where id=1')
+    expect(mockConnection.execute).toHaveBeenCalledWith('update table1 set f1="v1" where id=1', [])
   })
 
   test('deleteRec', async () => {
@@ -65,6 +66,6 @@ describe('dbutils', () => {
     
     const result = await deleteRec(query)
     expect(result).toBe(true)
-    expect(mockConnection.execute).toHaveBeenCalledWith('DELETE FROM table1 where id=1')
+    expect(mockConnection.execute).toHaveBeenCalledWith('DELETE FROM table1 where id=1', [])
   })
 })
